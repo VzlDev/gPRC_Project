3bee0d5d7684b5de9ddd61e889daf73a50b541f0
Tiago Teixeira <E1154@itsector.pt>
2024-06-28 16:10:20 +0100

    gprc project

--- ChatAppClient/obj/Debug/net6.0/ChatAppClient.AssemblyInfo.cs
+++ ChatAppClient/obj/Debug/net6.0/ChatAppClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2f086c5b395ba045fd15c7bec5741c4cef750b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
